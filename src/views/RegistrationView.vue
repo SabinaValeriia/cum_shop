5c9702b117c0812aa850488b4dcf776f4df88a02
SabinaValeriia <valeriia.sabina@onix-systems.com>
2023-03-28 14:09:17 +0300

    Add HomePage.vue

---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+.container 
+    .registration 
+        h2 Реєстрація 
+        form(type="submit")
+            input(placeholder="Ім'я")
+            button(@click.prevent="test")
+</template>
